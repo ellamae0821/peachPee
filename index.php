c1b9f43b8106d160258bd740fbeb34ef5f17d381
Ella Mae A <ellamae207@gmail.com>
2018-03-09 10:46:21 -1000

    adds thumbnail

--- index.php
+++ index.php
@@ -53,32 +53,37 @@
     </nav>
   </div>
   <div class="container">
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="img/car1.png" alt="https://i0.wp.com/wp.laravel-news.com/wp-content/uploads/2017/07/php-leader.png?resize=2200%2C1125">
-            <div class="carousel-caption">
-              PHP 1
-            </div>
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="img/car1.png" alt="php">
+          <div class="carousel-caption">
+            ...
           </div>
-          <div class="item">
-            <img src="img/car1.png" alt="https://i0.wp.com/wp.laravel-news.com/wp-content/uploads/2017/07/php-leader.png?resize=2200%2C1125">
-            <div class="carousel-caption">
-              PHP 2
-            </div>
+        </div>
+        <div class="item">
+          <img src="http://i.huffpost.com/gen/3152148/images/o-ANIMALS-FUNNY-facebook.jpg" alt="sweet cat">
+          <div class="carousel-caption">
+            ...
           </div>
-          WHAT AM I?
+        </div>
+        <div class="item">
+          <img src="https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/hires/2017/docatspurrwh.jpg" alt="gray cat">
+          <div class="carousel-caption">
+            ...
+          </div>
         </div>
 
-      <!-- Controls -->
+
+          <!-- Controls -->
         <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
@@ -87,7 +92,52 @@
           <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
+
       </div>
-  </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://www.techcoffees.com/wp-content/uploads/2018/02/bootstrap-wallpaper.png" alt="thumbnailphoto">
+        </a>
+      </div>
+    </div>
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/main.js"></script>
 </body>
 </html>
