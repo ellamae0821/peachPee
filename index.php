2ca1bca220fbbe74bdc593f1a34375d522d4e04f
Ella Mae A <ellamae207@gmail.com>
2018-03-09 03:49:30 -1000

    index.php init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PeachPee by Ella</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <!-- cd ~/.bitnami/stackman/machines/xampp/volumes/root/htdocs -->
+  TEST!
+</body>
+</html>
