b3ec3c48dea908d6c7883781037e3243a2dcd9da
Ella Mae A <ellamae207@gmail.com>
2018-03-09 04:33:58 -1000

    inserts carousel

--- index.php
+++ index.php
@@ -15,13 +15,21 @@
   <!-- cd ~/.bitnami/stackman/machines/xampp/volumes/root/htdocs -->
   FINALLY!
   <div class="container">
-    <div class="col-md-4">Logo</div>
-    <div class="col-md-8">Ad</div>
+    <div class="col-md-4"></div>
+    <div class="col-md-8"></div>
   </div>
   <div class="row">
     <nav class="navbar navbar-default">
       <div class="container-fluid">
-
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="img/ea_Logo.svg" height="42" width="42"></a>
+        </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
@@ -43,6 +51,43 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
+  </div>
+  <div class="container">
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="img/car1.png" alt="https://i0.wp.com/wp.laravel-news.com/wp-content/uploads/2017/07/php-leader.png?resize=2200%2C1125">
+            <div class="carousel-caption">
+              PHP 1
+            </div>
+          </div>
+          <div class="item">
+            <img src="img/car1.png" alt="https://i0.wp.com/wp.laravel-news.com/wp-content/uploads/2017/07/php-leader.png?resize=2200%2C1125">
+            <div class="carousel-caption">
+              PHP 2
+            </div>
+          </div>
+          WHAT AM I?
+        </div>
+
+      <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
   </div>
 </body>
 </html>
