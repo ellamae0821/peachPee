732ded7b930168ceb109545d8622fd379b4e2553
Ella Mae A <ellamae207@gmail.com>
2018-03-09 23:57:41 -1000

    admin page init

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>PeachPee by Ella</title>
+  <title>Admin Page</title>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
