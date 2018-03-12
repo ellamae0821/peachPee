8942655404041f4352e5f0719cdf9f4bcdac44d6
Ella Mae A <ellamae207@gmail.com>
2018-03-12 01:45:02 -1000

    fixes main index and connected php and mysql

--- index.php
+++ index.php
@@ -1,100 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>PeachPee by Ella</title>
+<?php
+session_start();
+require('config/connect.php');
+/*require('functionsfe.php');
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  $curpage = SetCurPage();
+  $perpage = PerPageResults();
+
+  $totalres = mysqli_num_rows(SelPublishedContent());
+
+  $pages = GetStartEndPages($curpage, $perpage, $totalres);
+
+  $start = $pages['start'];
+  $endpage = $pages['end'];
+
+  $res = SelPubContPag($start, $perpage);
+  $startpage = 1;
+  $nextpage = $curpage + 1;
+  $previouspage = $curpage - 1;
+*/
+?>
+
+<?php include 'inc/header.php' ?>
+<?php include 'inc/nav.php' ?>
 
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 
-  <script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-  <!-- cd ~/.bitnami/stackman/machines/xampp/volumes/root/htdocs -->
-
-  <div class="container">
-    <div class="col-md-4"></div>
-    <div class="col-md-8"></div>
-  </div>
-  <div class="row">
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">
-          <img src="img/ea_Logo.svg" height="42" width="42">
-        </a>
-      </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Travel<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Oahu</a></li>
-              <li><a href="#">Maui</a></li>
-              <li><a href="#">Kauai</a></li>
-              <li><a href="#">Big Island</a></li>
-            </ul>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vacation Packages <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Oahu</a></li>
-              <li><a href="#">Maui</a></li>
-              <li><a href="#">Kauai</a></li>
-              <li><a href="#">Big Island</a></li>
-            </ul>
-          </li>
-          <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Accommodations <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Oahu</a></li>
-            <li><a href="#">Maui</a></li>
-            <li><a href="#">Kauai</a></li>
-            <li><a href="#">Big Island</a></li>
-          </ul>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Things to Do <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Oahu</a></li>
-              <li><a href="#">Maui</a></li>
-              <li><a href="#">Kauai</a></li>
-              <li><a href="#">Big Island</a></li>
-            </ul>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Shop &amp; Dine <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Oahu</a></li>
-              <li><a href="#">Maui</a></li>
-              <li><a href="#">Kauai</a></li>
-              <li><a href="#">Big Island</a></li>
-            </ul>
-          </li>
-
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
-</div>
   </div>
   <div class="container">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
